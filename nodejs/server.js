2506ff364d527874e272a3cd4dbe6e02393e6bb0
Samuel Andre <samuelandredasilva@gmail.com>
2024-02-10 18:41:13 -0300

    Class 1 - Node server

---
+++ nodejs/server.js
@@ -0,0 +1,11 @@
+const http = require("http")
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('Node course')
+})
+
+server.listen(port, () => {
+    console.log(`🟢 Server is running on http://localhost:` + port)
+})
