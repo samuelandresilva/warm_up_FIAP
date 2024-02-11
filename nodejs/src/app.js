d7025613aaf80d8351b8feda8ae90564846dfe78
Samuel Andre <samuelandredasilva@gmail.com>
2024-02-11 12:25:55 -0300

    Class 5 - CRUD - Create

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 const books = [
     { id: 1, title: 'The Lord of The Rings' },
     { id: 2, title: 'The Hobbit' }
@@ -13,6 +15,11 @@ app.get('/', (req, res) => {
 
 app.get('/books', (req, res) => {
     res.status(200).json(books)
+})
+
+app.post('/books', (req, res) => {
+    books.push(req.body)
+    res.status(201).send()
 })
 
 export default app
