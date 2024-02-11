7650b7a2701c96c787acf8e89a8583d92c63287b
Samuel Andre <samuelandredasilva@gmail.com>
2024-02-11 10:01:35 -0300

    Class 2 - Local Server and Nodemon

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,9 +1,17 @@
 const http = require("http")
 const port = 3000
 
+const rotes = {
+    '/': 'Node course',
+    '/books': 'On books page',
+    '/authors': 'On authors page',
+    '/editors': 'On editors page',
+    '/about': 'About this project'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Node course')
+    res.end(rotes[req.url])
 })
 
 server.listen(port, () => {
